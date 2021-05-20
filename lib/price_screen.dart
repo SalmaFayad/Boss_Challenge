2d87107ea609395a84215e1cad61253779f46ec5
SalmaFayad <salmafayad123456789@gmail.com>
2021-05-20 17:26:30 +0200

    DropDownMenu Worked now!

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
-class PriceScreen extends StatelessWidget {
+import 'coin.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = currenciesList.first;
+  List<DropdownMenuItem> getDropDownItem() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropDownItems.add(newItem);
+    }
+    return dropDownItems;
+  }
+
   @override
   Widget build(BuildContext context) {
+    getDropDownItem();
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('🤑 Coin Ticker')),
@@ -19,13 +40,16 @@ class PriceScreen extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10.0),
               ),
               child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 28.0),
-                child: Text('1 BTC = ? USD',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
               ),
             ),
           ),
@@ -34,7 +58,14 @@ class PriceScreen extends StatelessWidget {
             height: 150,
             color: Colors.lightBlueAccent,
             padding: EdgeInsets.only(bottom: 30.0),
-            child: null,
+            child: DropdownButton<String>(
+                value: selectedCurrency,
+                items: getDropDownItem(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedCurrency = value;
+                  });
+                }),
           ),
         ],
       ),
